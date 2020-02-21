8748662a30e3a08f9d3bde2bf7a35012d7865021
Andy Campbell <47190204+acampbel@users.noreply.github.com>
2020-02-21 12:10:42 -0500

    Use html coverage report

---
+++ runAllTests.m
@@ -0,0 +1,21 @@
+import matlab.unittest.TestRunner;
+import matlab.unittest.Verbosity;
+import matlab.unittest.plugins.CodeCoveragePlugin;
+import matlab.unittest.plugins.XMLPlugin;
+import matlab.unittest.plugins.codecoverage.CoverageReport;
+
+cd('..');
+
+suite = testsuite(pwd, 'IncludeSubfolders', true);
+
+[~,~] = mkdir('code-coverage');
+[~,~] = mkdir('test-results');
+
+runner = TestRunner.withTextOutput('OutputDetail', Verbosity.Detailed);
+runner.addPlugin(XMLPlugin.producingJUnitFormat('test-results/results.xml'));
+runner.addPlugin(CodeCoveragePlugin.forFolder({'.'}, 'IncludingSubfolders', true, 'Producing', CoverageReport('code-coverage')));
+
+results = runner.run(suite);
+
+nfailed = nnz([results.Failed]);
+assert(nfailed == 0, [num2str(nfailed) ' tests failed.']);
